85bc2b8e42e50a3492820eee060f818e6bbf00b7
jeffrey-chr <jeffrey.t.chr@gmail.com>
2021-07-05 16:36:16 +1000

    Edit configuration script and gitignore.

--- configure.bat
+++ configure.bat
@@ -2,7 +2,7 @@
 
 REM Ensure that current directory is the location of this batch file.
 set mypath=%~dp0
-set curdir=%cd%
+for /f %%i in ('cd') do set curdir=%%i
 cd %mypath%
 
 REM Set up linkdir.txt
@@ -10,13 +10,19 @@ set argC=0
 for %%x in (%*) do Set /A argC+=1
 
 if %argC%==0 (
-    set /P loc="Enter location of utilities folder containing qap_readFile.m, etc: " 
+    if exist ..\Utilities\qap_readFile.m (
+        cd ..\Utilities\
+        for /f %%i in ('cd') do set loc=%%i
+        cd %mypath%
+    ) else (
+        set /P loc="Enter location of utilities folder containing qap_readFile.m, etc: " 
+    )
 ) else (
     set loc=%1
-    echo Setting utilities folder location as %loc%
 )
 
 REM Write the location of utilities in the first line of linkdir.txt
+echo Setting utilities folder location as %loc%
 echo %loc% > .\linkdir.txt
 
 REM Create any required folders
